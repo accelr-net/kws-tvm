b54170103c3f66bc9a50a5ef0cfb73edbbaef9ed
Benn <broshnan@analog-inference.com>
2023-08-08 13:40:25 +0530

    unoptimzed inferencing added and some moduled changes

--- kws/rn18/kwsrn18pytorch.py
+++ kws/rn18/kwsrn18pytorch.py
@@ -46,12 +46,12 @@ def load_model(mdl_path: str):
 
     try:
         model = torch.jit.load(mdl_path)
-        print("Model loaded successfully.")
+        print(" Model loaded successfully.")
         print("\n")
         return model
     
     except Exception as e:
-        print(f"Error loading the model: {e}")
+        print(f" Error loading the model: {e}")
         print("\n")
         return None
 
@@ -72,11 +72,13 @@ def transform_audio(audio, sample_rate: int):
     transformed = transform(audio)
     return transformed
 
+
 def get_shape():
     set = SubsetSC("testing")
     waveform, sample_rate, *_ = set[0]
     spectogram = mel_conversion(waveform,sample_rate)
     return spectogram.shape
+
 
 def reshape(waveform,sample_rate):
     if waveform.shape[-1] < sample_rate :
@@ -86,43 +88,95 @@ def reshape(waveform,sample_rate):
     waveform = transform_audio(waveform, sample_rate)
     return waveform
 
+
 def mel_conversion(waveform,sample_rate):
     waveform = reshape(waveform,sample_rate)
     spectogram = ToTensor()(librosa.power_to_db(waveform.squeeze().numpy(), ref=np.max))
     spectogram = spectogram.unsqueeze(0)
     return spectogram
 
-def main(args):
 
-    # Loading the Pytorch Model (CUDA/CPU) 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("\n")
-    print(" ----------------------------")
-    print("| Loading Pre Trained Model |")
-    print(" ----------------------------")
-    print("\n")
-
-    model = load_model(args.model_path)
-    
-
-    labels = load_labels(args.lbl_path)
+def dataset_inference(dataset,dev_model,labels):
 
     CLASS_TO_IDX = {c: i for i, c in enumerate(labels)}
     idx_to_class = {v: k for k, v in CLASS_TO_IDX.items()}
+    count = 0
+    correct = 0
+    wrong = 0
+    start_time = time.time()
+
+    for t in dataset:
+
+        waveform_t, sample_rate_t, label_t, speaker_id_t, utterance_number_t = t
+        spectogram_t = mel_conversion(waveform_t,sample_rate_t)
+        input_name_t = "input0" 
+        dev_model.set_input(input_name_t, tvm.nd.array(spectogram_t))
+        dev_model.run()
+        tvm_out = dev_model.get_output(0)
+        top1_tvm = np.argmax(tvm_out.numpy()[0])
+        top1_tvm_label = idx_to_class[top1_tvm]
+
+        count = count + 1
+        if(top1_tvm_label == label_t):
+            correct = correct + 1
+        else:
+            wrong = wrong + 1 
+
+    total_time = time.time()-start_time
+    speed = count/total_time
+    accuracy = correct/count
+
+    return speed, accuracy
+
+
+def pytorch_inference(dataset,model,labels):
+
+    CLASS_TO_IDX = {c: i for i, c in enumerate(labels)}
+    idx_to_class = {v: k for k, v in CLASS_TO_IDX.items()}
+    count = 0
+    pycorrect = 0
+    pywrong = 0
+    start_time = time.time()
+
+    for t in dataset:
+        waveform_t, sample_rate_t, label_t, speaker_id_t, utterance_number_t = t
+        spectogram_t = mel_conversion(waveform_t,sample_rate_t)
+
+        
+        pred = torch.argmax(model(spectogram_t), dim=1)
+        pytorch_pred = idx_to_class[pred.item()]
+
+        count = count + 1
+
+        if(pytorch_pred == label_t):
+            pycorrect = pycorrect + 1
+        else:
+            pywrong = pywrong + 1 
+    
+    total_time = time.time()-start_time
+    speed = count/total_time
+    accuracy = pycorrect/count
+   
+    return speed, accuracy
+
+
+
+def main(args):
+
+    print("\n")
+    print(" Loading Pre Trained Model ...")
+
+    # Loading the Pytorch Model (CUDA/CPU) 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = load_model(args.model_path)
+    labels = load_labels(args.lbl_path)
 
     # Insertion of data set for inference
-    
-
     test_set = SubsetSC("testing")
     waveforms = []
     sample_rates = []
-    labels = []
     speaker_ids = []
     utterance_numbers = []
-    print(" ------------------------")
-    print("| Starting Optimization |")
-    print(" ------------------------")
-    print("\n")
 
     # Importing the ML Graph to Relay (TVM)
     input_shape = get_shape()
@@ -131,13 +185,28 @@ def main(args):
     mod, params = relay.frontend.from_pytorch(model, shape_list)
     target = tvm.target.Target("llvm") # Optimization will happen to target device given here.
 
+    # Executing the unoptimized graph on the implemented Device
+    with tvm.transform.PassContext(opt_level=3, config={}):
+            lib = relay.build(mod, target=target, params=params)
+    dev = tvm.cpu(0)
+    uo_m = graph_executor.GraphModule(lib["default"](dev))
+
+    print(" Running TVM Unoptimized Inferencing ...")
+
+    # Inferencing with ML network implemented in device
+    uo_speed,uo_accuracy = dataset_inference(test_set,uo_m,labels)
+    print(" Unoptimized Inference Completed.")
+    print("\n")
+
+    print(" Starting Optimization ...")
+    print("\n")
 
     ## Auto tuning Optimization
 
-    number = 10 # specifies the number of different configurations
-    repeat = 1 # how many measurements we will take of each configuration
+    number = 100 # specifies the number of different configurations
+    repeat = 10 # how many measurements we will take of each configuration
     min_repeat_ms = 0  # how long need to run configuration test. If the number of repeats falls under this time, it will be increased , since we're tuning on a CPU, can be set to 0
-    timeout = 10  # in seconds, upper limit on how long to run training code for each tested configuration
+    timeout = 60  # in seconds, upper limit on how long to run training code for each tested configuration
 
     # create a TVM runner
     runner = autotvm.LocalRunner(
@@ -158,18 +227,15 @@ def main(args):
     "tuning_records": "kws-rn18-autotuning.json",
     }
 
-    print("** Extracting Tasks.... ")
+    print(" Extracting Tasks ... ")
     print("\n")
 
     # begin by extracting the tasks from the ML model
     tasks = autotvm.task.extract_from_program(mod["main"], target=target, params=params)
     pprint.pprint(tasks)
 
-
-
-
     print("\n")
-    print("** Tuning Extracted Tasks.... ")
+    print(" Tuning Extracted Tasks ... ")
     print("\n")
 
     # Tune the extracted tasks sequentially.
@@ -190,94 +256,48 @@ def main(args):
     with autotvm.apply_history_best(tuning_option["tuning_records"]):
         with tvm.transform.PassContext(opt_level=3, config={}):
             lib = relay.build(mod, target=target, params=params)
-    dev = tvm.cpu(0)
 
-
-    print("\n")
-    print(" -----------------------")
-    print("| Running Inferencing |")
-    print(" -----------------------")
-    # Performance Variables
-    count = 0
-    pcount = 0
-    correct = 0
-    wrong = 0 
-    pycorrect = 0
-    pywrong = 0
-
-    # Executing the graph on the implemented Device
-    m = graph_executor.GraphModule(lib["default"](dev))
-
-    #Performance Measurement of the Data set
-
-    #Inferencing with Pytorch and it's performance measurement
-
-    pstart_time = time.time()
-
-    for t in test_set:
-        waveform_t, sample_rate_t, label_t, speaker_id_t, utterance_number_t = t
-        spectogram_t = mel_conversion(waveform_t,sample_rate_t)
-
-        
-        pred = torch.argmax(model(spectogram_t), dim=1)
-        pytorch_pred = idx_to_class[pred.item()]
-
-        pcount = pcount + 1
-
-        if(pytorch_pred == label_t):
-            pycorrect = pycorrect + 1
-        else:
-            pywrong = pywrong + 1 
-        
-    pyaccuracy = pycorrect/pcount
-
-    print("\n")
-    print("----------- Inferencing performance with Pytorch  ------------")
-    print("\n")
-    print("Count: " + str(pcount) + " Correct: " + str(pycorrect) + " Wrong: " + str(pywrong))
-    print("Accuaracy: " + str(pyaccuracy))
-    print('Inference speed: %.2f samples/s'%(pcount/(time.time()-pstart_time)))
-    print("\n")
-
-    #Inferencing with ML network implemented in llvm optimized via TVM
-
-    start_time = time.time()
-
-    for l in test_set:
-
-        waveform_t, sample_rate_t, label_t, speaker_id_t, utterance_number_t = l
-        spectogram_t = mel_conversion(waveform_t,sample_rate_t)
-
-        input_name_t = "input0" 
-        m.set_input(input_name_t, tvm.nd.array(spectogram_t))
-        m.run()
-        tvm_out = m.get_output(0)
-        top1_tvm = np.argmax(tvm_out.numpy()[0])
-        top1_tvm_label = idx_to_class[top1_tvm]
-
-        count = count + 1
-        if(top1_tvm_label == label_t):
-            correct = correct + 1
-        else:
-            wrong = wrong + 1 
-
-
-    accuracy = correct/count
+    print(" Running TVM Optimized Inferencing ...")
     
-    print("----------- Inferencing performance with ML network implemented in llvm optimized via TVM  ------------")
+    # Executing the optimized graph on the implemented Device
+    o_m = graph_executor.GraphModule(lib["default"](dev))
+
+    # Inferencing with ML network implemented in llvm optimized via TVM
+    o_speed,o_accuracy = dataset_inference(test_set,o_m,labels)
+
+    print(" Optimized Inference Completed.")
     print("\n")
-    print("Count: " + str(count) + " Correct: " + str(correct) + " Wrong: " + str(wrong))
-    print("Accuaracy: " + str(accuracy))
-    print('Inference speed: %.2f samples/s'%(count/(time.time()-start_time)))
+
+    print(" Running Pytorch Inferencing ... ")
+    
+    # Inferencing with Pytorch and it's performance measurement
+    pyspeed, pyaccuracy  = pytorch_inference(test_set,model,labels)
+
+    print(" Optimized Inference Completed.")
     print("\n")
 
-    
+    ## Performance Stats Printing
 
-    
+    print("-----------------------")
+    print(" ~ Performance Stats ~ ")
+    print("-----------------------")
 
+    print("\n")
+    print("Pytorch Accuaracy: " + str(pyaccuracy))
+    print('Pytorch Inference speed: %.2f samples/s'%(pyspeed))
+    print("\n")
+
+    print("TVM Accuaracy: " + str(uo_accuracy))
+    print('Unoptimized TVM implementation Inference speed: %.2f samples/s'%(uo_speed))
+    print("\n")
     
-   
+    print('Optimized TVM Inference speed: %.2f samples/s'%(o_speed))
+    print("\n")
+    print("\n")
     
+    print(" ~ End of Script ~")
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
